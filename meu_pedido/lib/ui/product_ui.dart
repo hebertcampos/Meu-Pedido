37b87df2044ab3c902b003617668681965e250c4
hebert <hebertcampos@hotmail.com.br>
2019-05-16 20:45:35 -0300

    colocando as caracteristicas do produto

--- meu_pedido/lib/ui/product_ui.dart
+++ meu_pedido/lib/ui/product_ui.dart
@@ -12,12 +12,13 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+  String size;
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-
     final Color primaryColor = Theme.of(context).primaryColor;
+    print(product.sizes.length);
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
@@ -28,7 +29,7 @@ class _ProductScreenState extends State<ProductScreen> {
           AspectRatio(
             aspectRatio: 0.9,
             child: Carousel(
-              images: product.images.map((url){
+              images: product.images.map((url) {
                 return NetworkImage(url);
               }).toList(),
               dotSize: 4.0,
@@ -45,19 +46,95 @@ class _ProductScreenState extends State<ProductScreen> {
               children: <Widget>[
                 Text(
                   product.title,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold
-                  ),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                   maxLines: 3,
                 ),
                 Text(
                   'R\$ ${product.price.toStringAsFixed(2)}',
                   style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Tamanho',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5,
+                    ),
+                    children: product.sizes.map((s) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            size = s;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0)),
+                              border: Border.all(
+                                  color: s == size
+                                      ? primaryColor
+                                      : Colors.grey[500],
+                                  width: 3.0)),
+                          width: 50.0,
+                          alignment: Alignment.center,
+                          child: Text(s),
+                        ),
+                      );
+                    }).toList(),
                   ),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                    height: 44.0,
+                    child: product.sizes.length > 1
+                        ? RaisedButton(
+                            onPressed: size != null ? () {} : null,
+                            child: Text(
+                              'Adicionar ao carrinho',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            color: primaryColor,
+                            textColor: Colors.white,
+                          )
+                        : RaisedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Adicionar ao carrinho',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            color: primaryColor,
+                            textColor: Colors.white,
+                          )),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  'Descrição',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
                 )
               ],
             ),
